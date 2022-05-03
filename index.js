4984035ee0c4e13b1ffefdbcac2dc4a501225c4d
Michelle <clairsin@gmail.com>
2022-05-03 15:42:57 -0400

    commit tree 5d3d47f20635bd7144342568caf0e6e0cc53f302

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Add your code here
+
+// const { config } = require("chai")
+
+
+  function submitData(userName, userEmail) {
+
+    const configObject = {
+      method: "POST",
+      headers: {
+        'Content-Type': "application/json",
+        'Accept': "application/json",
+      },
+      body: JSON.stringify({
+        name: `${userName}`,
+        email: `${userEmail}`,
+      })
+    }
+
+    return fetch('http://localhost:3000/users', configObject)
+      .then (res => res.json())
+      .then (data => {
+        const userID = data.id
+        document.querySelector('body').append(userID)
+        console.log(data)
+  })
+      .catch((error => {
+        alert('There was an error!')
+        document.querySelector('body').append(error.message)
+      }))
+  }
+submitData("testUser", "testUser@test.com")
